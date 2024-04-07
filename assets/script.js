f437cfe24176e9abe84ad8af1941afd3faa99471
Nathalie <ngregoirecfsc@gmail.com>
2024-04-07 10:25:39 -0400

    Ajout des dots html et début de code JS

--- assets/script.js
+++ assets/script.js
@@ -16,39 +16,71 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
-console.log(slides);
+//console.log(slides);
 
-let position = 0; 
-const numberOfSlide = slides.length;
+let currentIndex = 0;
+const numberOfSlides = slides.length;
 
-createCaroussel(position);
 
 
 
 const left = document.querySelector('.arrow_left');
 const right = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); // Sélectionne tous les points
 
 
-
-
+// Chevrons
 left.addEventListener("click", function () {
-    if (position == 0) {
-        position = numberOfSlide - 1;
+    if (index == 0) {
+        index = numberOfSlides- 1;
     }
     else {
-        position--;
+        index--;
     }
-        createCaroussel(position);
+    
 });
 
 right.addEventListener("click", function () {
-    if (position == numberOfSlide - 1) {
-        position = 0;
-    } else {
-            position++;
+    if (index == numberOfSlides - 1) {
+        index = 0;
+    } 
+	else {
+        index++;
     }
-    createCaroussel(position);
+ 
 
 });
 
+// Dots
+// Fonction pour mettre à jour les points indicateurs
+function updateDots(index) {
+    dots.forEach((dot, i) => {
+        if (i === index) {
+            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
+        } else {
+            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
+        }
+    });
+}
+
+// Fonction pour mettre à jour les points indicateurs, l'image et le texte
+function updateCarousel(index, direction) {
+      //correction du bug pour la première et la dernière image
+    if (currentIndex === -1 && direction === 'left') {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex === slides.length && direction === 'right') {
+        currentIndex = 0;
+    }
+
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+    bannerImg.src = imagePath;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+
+    // Mettre à jour le texte
+    const tagLine = slides[currentIndex].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
 
