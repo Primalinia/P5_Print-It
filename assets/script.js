0d9b665cc7e141af56843008c2f3ae61f432ad53
Nathalie <ngregoirecfsc@gmail.com>
2024-04-11 09:03:15 -0400

    Amelioration des commentaires

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Variables
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -18,69 +19,59 @@ const slides = [
 ];
 //console.log(slides);
 
-let currentIndex = 0;
-const numberOfSlides = slides.length;
-
-
-
-
 const left = document.querySelector('.arrow_left');
 const right = document.querySelector('.arrow_right');
+const bannerImg = document.querySelector('.banner-img');
 const dots = document.querySelectorAll('.dot'); // Sélectionne tous les points
-
+let currentIndex = 0;
 
 // Chevrons
-left.addEventListener("click", function () {
-    if (index == 0) {
-        index = numberOfSlides- 1;
-    }
-    else {
-        index--;
-    }
-    
+/* Gestionnaire d'événement,
+au clic du chevron gauche */
+left.addEventListener('click', function () {
+    currentIndex = (currentIndex - 1);
+    updateCarousel(currentIndex, 'left');
+    updateDots(currentIndex); // actualise les dots
 });
 
-right.addEventListener("click", function () {
-    if (index == numberOfSlides - 1) {
-        index = 0;
-    } 
-	else {
-        index++;
-    }
- 
-
+// Gestionnaire d'événement au clic du chevron droit
+right.addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) ;
+    updateCarousel(currentIndex, 'right');
+    updateDots(currentIndex); // Actualise les dots
 });
 
 // Dots
-// Fonction pour mettre à jour les points indicateurs
+/* Fonction pour mettre à jour,
+les points indicateurs */
 function updateDots(index) {
     dots.forEach((dot, i) => {
         if (i === index) {
-            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
+            dot.classList.add('dot_selected'); // Ajoute la classe pour le point actuel
         } else {
-            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
+            dot.classList.remove('dot_selected'); // Supprime la classe pour les autres points
         }
     });
 }
 
-// Fonction pour mettre à jour les points indicateurs, l'image et le texte
+//Mises à jour
+/* Fonctions de mises à jour: 
+Image, texte et points indicateurs */
 function updateCarousel(index, direction) {
-      //correction du bug pour la première et la dernière image
-    if (currentIndex === -1 && direction === 'left') {
+      // Passage entre la première et la dernière image du caroussel
+    if (currentIndex === -1 && direction === 'left') { 
         currentIndex = slides.length - 1;
     } else if (currentIndex === slides.length && direction === 'right') {
         currentIndex = 0;
     }
 
-    // Mettre à jour l'image
+	/* Mise à jour: 
+	de l'image */
     const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
     bannerImg.src = imagePath;
-    bannerImg.alt = `Slide ${currentIndex + 1}`;
+    bannerImg.alt = `Slide ${currentIndex + 1}`; // $ selectionne l'élément
 
-    // Mettre à jour le texte
+    /* Mise  à jour du texte */
     const tagLine = slides[currentIndex].tagLine;
     document.querySelector('p').innerHTML = tagLine;
-
-    console.log(`Clic sur la flèche ${direction}`);
 }
-
